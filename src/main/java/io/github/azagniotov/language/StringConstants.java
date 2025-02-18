090d5c3c3161c7ebf0b7f754a160a8c709b55032
Alexander Zagniotov <azagniotov@gmail.com>
2025-02-17 20:48:34 -0800

    [MINOR] Using circular buffer during n-gram extraction from input
    During n-gram extraction, no longer relying on StringBuilder as the n-gram
    buffer to store the chars. Instead, using a char[] array as a circular buffer

--- src/main/java/io/github/azagniotov/language/StringConstants.java
+++ src/main/java/io/github/azagniotov/language/StringConstants.java
@@ -2,9 +2,9 @@ package io.github.azagniotov.language;
 
 class StringConstants {
 
+  static final char BLANK_CHAR = ' ';
   static final String EMPTY_STRING = "";
   static final String BLANK_SPACE = " ";
-  static final char BLANK_CHAR = ' ';
   static final String COMMA_CHAR = ",";
   static final String TAB_CHAR = "\t";
 
